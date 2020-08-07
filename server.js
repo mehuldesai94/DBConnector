ddedd98bf91e7046eb1d62abe1646601a3788527
Mehul Desai <desaimehul94@gmail.com>
2020-08-07 17:28:56 -0400

    CRUD backend completed.

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ userRoute.route('/').get((req, res) => {
     });
 });
 
-userRoute.route('/add').post((req, res) => {
+userRoute.route('/addUser').post((req, res) => {
     let user = new userTable(req.body);
     console.log("add path called");
     user.save()
@@ -41,7 +41,69 @@ userRoute.route('/add').post((req, res) => {
         });
 });
 
-app.use('/user', userRoute);
+userRoute.route('/:id').get((req, res) => {
+    let userId = req.params.id;
+    userTable.findById(userId, (err, userData) => {
+        res.json(userData);
+    })
+})
+
+
+
+// userRoute.route('/userupdate/:id').post((req, res) => {
+//     console.log(req.params.id);
+//     userTable.findById(req.params.id, (err, user) => {
+//         if (!user)
+//             res.send(404).send('User not avaliable');
+//         else {
+//             user.firstName = req.body.firstName;
+//             user.lastName = req.body.lastName;
+//             user.phone = req.body.phone;
+//             user.email = req.body.email;
+//             user.street1 = req.body.street1;
+//             user.street2 = req.body.street2;
+//             user.city = req.body.city;
+//             user.province = req.body.province;
+//             user.country = req.body.country;
+//         }
+
+//         user.save().then(user => {
+//             res.json('User upadated succesfully');
+//         }).catch(err => {
+//             res.status(400).send("Update failed ... please try again later on");
+//         });
+//     })
+// })
+
+userRoute.route('/updateuser/:id').post((req, res) => {
+    let userId = req.params.id;
+    console.log("User update function called.. and id is : " + userId);
+
+    userTable.findById(userId, (err, userData) => {
+        if(!userData)
+            res.send(404).send("data is not found");
+        else{
+            userData.firstName = req.body.firstName;
+            userData.lastName = req.body.lastName;
+            userData.phone = req.body.phone;
+            userData.email = req.body.email;
+            userData.street1 = req.body.street1;
+            userData.street2 = req.body.street2;
+            userData.city = req.body.city;
+            userData.province = req.body.province;
+            userData.country = req.body.country;
+        }
+
+        userData.save().then(userData => {
+            res.json('User Updated!');
+        })
+        .catch(err => {
+            res.status(400).send("update user not possible");
+        })
+    })
+})
+
+app.use('/users', userRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
