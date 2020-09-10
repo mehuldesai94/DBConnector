af917b8459e8662bbbbd68461ca56d4730f19abc
Mehul Desai <desaimehul94@gmail.com>
2020-09-10 13:23:17 -0400

    gmail mailer connected.

---
+++ Routes/nodemailerRoutes.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const config = require('../config/config');
+const nodemailerRoute = express.Router();
+
+
+const gmailtransport = {
+    host: 'smtp.gmail.com', // Don’t forget to replace with the SMTP host of your provider
+    port: 587,
+    auth: {
+        user: config.USER,
+        pass: config.USER_CODE
+    }
+}
+
+const gmailtrnsporter = nodemailer.createTransport(gmailtransport);
+
+gmailtrnsporter.verify((error, success) => {
+    if (error)
+        console.log(error);
+    else
+        console.log('Server is ready to send message');
+});
+
+nodemailerRoute.post('/send', (req, res, next) => {
+    var name = req.body.name;
+    var email = req.body.email;
+    var phone = req.body.phone;
+    var message = req.body.message;
+    var content = `name: ${name}
+                    \n email: ${email}
+                    \n phone: ${phone}
+                    \n message: ${message}`
+
+    var mail = {
+        from: name,
+        to: config.USER,
+        subject: 'New email from Portfolio',
+        text: content
+    }
+
+    gmailtrnsporter.sendMail(mail, (err, data) => {
+        if (err) {
+            res.json({
+                status: 'fail'
+            })
+        } else {
+            res.json({
+                status: 'success'
+            })
+        }
+    })
+})
+
+module.exports = nodemailerRoute;
--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const cors = require('cors'); //help to call over http, https
 const bodyParser = require('body-parser'); //parsing data
 const morgan = require('morgan'); //server status
 
+
 const userRoute = require('./Routes/userRoutes');
+const nodemailerRoute = require('./Routes/nodemailerRoutes');
 const config = require('./config/config')
+
 
 const app = express();
 const PORT = process.env.PORT || config.PORT;
@@ -23,14 +26,22 @@ app.use(
   );
   
 app.use(bodyParser.json());
+app.use(express.json());
 
 //Routes
 app.use('/users', userRoute);
+
+
+//nodemailer
+app.use('/mail', nodemailerRoute)
+
 
 //Home Route
 app.get("/", (req, res) => {
     res.send("Welcome to Mehul's main NODE.JS API...");
   });
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
