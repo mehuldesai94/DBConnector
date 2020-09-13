b2c0ecc982396874365ece2daa1b0b345993c5b3
Mehul Desai <desaimehul94@gmail.com>
2020-09-12 22:02:26 -0400

    config file updated

--- config/config.js
+++ config/config.js
@@ -3,5 +3,5 @@ module.exports = {
     PORT: '4000',
     MONGO_URI: 'mongodb+srv://mehul:mehul@cluster0.zccmv.mongodb.net/mernerudapp?retryWrites=true&w=majority',
     USER: 'desaimehulkumar94@gmail.com',
-    USER_CODE: 'Mehul@9894'
+    USER_CODE: 'rfmjolmsmrmelybb'
 }
